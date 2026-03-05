e1113f81bd71e937c2fca2c52b9f877eabdb48ed
tanuj308 <tanujkanetiya308@gmail.com>
2026-03-05 08:40:13 +0530

    og is back

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ const voteRoutes = require('./routes/voteRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://spygameog.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
